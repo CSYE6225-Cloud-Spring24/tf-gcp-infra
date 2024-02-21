33cde54e3a2009de20d168a197119be50d0687af
Keerthana Mikkili <mikkili.k@northeastern.edu>
2024-02-21 07:05:40 -0500

    Added VM

--- main.tf
+++ main.tf
@@ -1,3 +1,99 @@
+# terraform {
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "4.51.0"
+#     }
+#   }
+# }
+
+# provider "google" {
+#   project = var.project_id
+#   region  = var.region
+#   zone    = var.zone
+# }
+# resource "google_compute_network" "vpc_network" {
+#   count                           = length(var.vpcs)
+#   name                            = var.vpcs[count.index].vpc_name
+#   auto_create_subnetworks         = var.autocreatesubnets
+#   routing_mode                    = var.routingmode
+#   delete_default_routes_on_create = var.deletedefaultroutes
+# }
+
+# resource "google_compute_subnetwork" "webapp" {
+#   count                    = length(var.vpcs)
+#   name                     = var.vpcs[count.index].websubnet_name
+#   ip_cidr_range            = var.vpcs[count.index].webapp_subnet_cidr
+#   region                   = var.region
+#   network                  = google_compute_network.vpc_network[count.index].self_link
+#   private_ip_google_access = var.vpcs[count.index].privateipgoogleaccess
+# }
+# resource "google_compute_subnetwork" "db" {
+#   count                    = length(var.vpcs)
+#   name                     = var.vpcs[count.index].dbsubnet_name
+#   ip_cidr_range            = var.vpcs[count.index].db_subnet_cidr
+#   region                   = var.region
+#   network                  = google_compute_network.vpc_network[count.index].self_link
+#   private_ip_google_access = var.vpcs[count.index].privateipgoogleaccess
+# }
+
+# resource "google_compute_route" "webapp_route" {
+#   count            = length(var.vpcs)
+#   name             = var.vpcs[count.index].websubnetroutename
+#   network          = google_compute_network.vpc_network[count.index].self_link
+#   dest_range       = var.webapp_subnetroute_cidr
+#   priority         = 1000
+#   next_hop_gateway = var.nexthopgateway
+# }
+
+# resource "google_compute_firewall" "allowtraffic_applicationport" {
+#   name    = "allowtraffic_applicationport"
+#   network = google_compute_network.vpc_network[count.index].name
+
+#   allow {
+#     protocol = "tcp"
+#     ports    = [var.app_port]
+#   }
+
+#   source_ranges = ["0.0.0.0/0"]
+# }
+
+# resource "google_compute_firewall" "denytraffic_sshport" {
+#   name    = "denytraffic-sshport"
+#   network = google_compute_network.vpc_network[count.index].name
+
+#   deny {
+#     protocol = "tcp"
+#     ports    = ["22"]
+#   }
+
+#   source_ranges = ["0.0.0.0/0"]
+# }
+
+# resource "google_compute_instance" "vm_instance" {
+#   name         = var.vm_name
+#   zone         = var.zone
+#   machine_type = var.vm_machine_type
+
+#   boot_disk {
+#     initialize_params {
+#       image = var.vm_image
+#       type  = var.vm_disk_type
+#       size  = var.vm_disk_size_gb
+#     }
+#   }
+
+#   network_interface {
+#     network    = google_compute_network.vpc_network[count.index].name
+#     subnetwork = google_compute_subnetwork.webapp[count.index].name
+
+#     access_config {
+#       // Assigns a public IP address
+#     }
+#   }
+# }
+
+
 terraform {
   required_providers {
     google = {
@@ -10,8 +106,9 @@ terraform {
 provider "google" {
   project = var.project_id
   region  = var.region
-  zone    = var.region
+  zone    = var.zone
 }
+
 resource "google_compute_network" "vpc_network" {
   count                           = length(var.vpcs)
   name                            = var.vpcs[count.index].vpc_name
@@ -28,6 +125,7 @@ resource "google_compute_subnetwork" "webapp" {
   network                  = google_compute_network.vpc_network[count.index].self_link
   private_ip_google_access = var.vpcs[count.index].privateipgoogleaccess
 }
+
 resource "google_compute_subnetwork" "db" {
   count                    = length(var.vpcs)
   name                     = var.vpcs[count.index].dbsubnet_name
@@ -45,4 +143,58 @@ resource "google_compute_route" "webapp_route" {
   priority         = 1000
   next_hop_gateway = var.nexthopgateway
 }
+
+resource "google_compute_firewall" "allowtraffic_applicationport" {
+  count   = length(var.vpcs)
+  name    = "allow-traffic-application-port-${count.index}" // Update the name to match the required format
+  network = google_compute_network.vpc_network[count.index].name
+
+  allow {
+    protocol = "tcp"
+    ports    = [var.app_port]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "denytraffic_sshport" {
+  count   = length(var.vpcs)
+  name    = "deny-traffic-ssh-port-${count.index}" // Update the name to match the required format
+  network = google_compute_network.vpc_network[count.index].name
+
+  deny {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  count = length(var.vpcs)
+
+  name         = var.vm_name
+  zone         = var.zone
+  machine_type = var.vm_machine_type
+
+  boot_disk {
+    initialize_params {
+      image = var.vm_image
+      type  = var.vm_disk_type
+      size  = var.vm_disk_size_gb
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network[count.index].name
+    subnetwork = google_compute_subnetwork.webapp[count.index].name
+
+    access_config {
+      // Assigns a public IP address
+    }
+  }
+}
+
+
 
